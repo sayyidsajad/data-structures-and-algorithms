0aa9336fee9df4e4f35142d2ef3f4f2a0249f8b9
sayyidsajad <sayyidsajada4s@gmail.com>
2024-01-10 09:43:11 +0530

    added graph data structures

---
+++ graph.js
@@ -0,0 +1,49 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = new Set();
+    }
+  }
+
+  addEdges(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1]) {
+      this.addVertex(vertex1);
+    }
+    if (!this.adjacencyList[vertex2]) {
+      this.addVertex(vertex2);
+    }
+    this.adjacencyList[vertex1].add(vertex2);
+    this.adjacencyList[vertex2].add(vertex1);
+  }
+
+  hasEdge(vertex1, vertex2) {
+    return (
+      this.adjacencyList[vertex1].has(vertex2) &&
+      this.adjacencyList[vertex2].has(vertex1)
+    );
+  }
+
+  removeEdges(vertex1, vertex2) {
+    this.adjacencyList[vertex1].delete(vertex2);
+    this.adjacencyList[vertex2].delete(vertex1);
+  }
+
+  removeVertex(vertex) {
+    if (!this.adjacencyList) {
+      return;
+    }
+    for (let adj of this.adjacencyList[vertex]) {
+      this.removeVertex(vertex, adj);
+    }
+    delete this.adjacencyList[vertex];
+  }
+
+  display() {
+    for (let vertex of this.adjacencyList[vertex]) {
+      console.log(vertex + "-->" + [...this.adjacencyList[vertex]]);
+    }
+  }
+}
